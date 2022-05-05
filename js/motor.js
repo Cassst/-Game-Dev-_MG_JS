b39150f5dea354034577ba6b7284517f619b869b
Augusto Castro <augusto.94.castro@gmail.com>
2022-05-05 20:30:22 -0300

    canvas

---
+++ js/motor.js
@@ -0,0 +1,62 @@
+
+
+let loop = {
+    idEjecucion: null,
+    ultimoRegistro: 0,
+    aps: 0,
+    fps: 0,
+
+    iterar: function(registroTemporal){
+        loop.idEjecucion = window.requestAnimationFrame( loop.iterar );
+        loop.update( registroTemporal );
+        loop.play();
+
+        if( registroTemporal - loop.ultimoRegistro > 999 ){
+            loop.ultimoRegistro = registroTemporal;
+            console.log(loop.fps);
+            loop.aps = 0;
+            loop.fps = 0;
+        };
+    },
+
+    update: function(){
+        loop.aps++;
+    },
+
+    play: function(){
+        loop.fps++;
+        borrarCanvas();
+    },
+
+};
+
+//Ventana de renderización
+let canvas = document.getElementById( "canvas" );
+let ctx = canvas.getContext('2d');
+let margen = 10;
+let canvasTop = (  margen / 2 );
+let canvasLeft = ( margen  / 2 );
+let canvasWidth = window.innerWidth-margen;
+let canvasHeight = window.innerHeight-margen;
+
+function borrarCanvas(){
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+  }
+
+//Canvas Responsive
+function ResizeWindow(){
+    canvas.style.top = canvasTop + "px";
+    canvas.style.left = canvasLeft + "px";
+    canvas.style.width = canvasWidth + "px";
+    canvas.style.height = canvasHeight + "px";
+};
+
+window.addEventListener( "load", function ( e ){
+    ResizeWindow();
+    loop.iterar();
+})
+
+window.addEventListener( "resize", function( e ) {
+    ResizeWindow();
+});
